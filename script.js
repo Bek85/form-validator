739b38273952dd56c7e30f72237034c7423af9e5
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-12-28 21:59:49 +0500

    create checkPasswordMatch function

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ const form = document.getElementById('form'),
   password = document.getElementById('password'),
   confirmPassword = document.getElementById('confirmPassword');
 
+const inputFieldNames = {
+  username: 'Username',
+  confirmPassword: 'Confirm Password',
+  password: 'Password',
+  email: 'E-mail',
+};
+
 const showError = (input, message) => {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
@@ -16,27 +23,65 @@ const showSuccess = (input) => {
   formControl.className = 'form-control success';
 };
 
-const isValidEmail = (email) => {
+const checkEmail = (input) => {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 };
 
 const getFieldName = (input) => {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  return inputFieldNames[input.id]
+    ? inputFieldNames[input.id]
+    : input.id.charAt(0).toUpperCase() + input.id.slice(1);
 };
 
 const checkRequired = (inputArr) => {
+  let isRequired = false;
   inputArr.forEach((input) => {
     if (input.value.trim() === '') {
       showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
     } else {
       showSuccess(input);
     }
   });
+  return isRequired;
+};
+
+const checkLength = (input, min, max) => {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be less than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+};
+
+const checkPasswordMatch = (input1, input2) => {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords do not match');
+  }
 };
 
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
-  checkRequired([username, email, password, confirmPassword]);
+  if (checkRequired([username, email, password, confirmPassword])) {
+    return;
+  } else {
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 20);
+    checkEmail(email);
+    checkPasswordMatch(password, confirmPassword);
+  }
 });
